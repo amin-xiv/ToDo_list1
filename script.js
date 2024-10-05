9ef205393c7c140c96684145de2a67954b9eb5ba
Amin Jalal <mo13241234@gmail.com>
2024-10-05 12:27:39 +0300

    UI css design and added 'Delete task' functionality.

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const addButton = document.getElementById("add-task-btn");
 const taskContainer = document.getElementById("task-container");
 const taskField = document.getElementById("new-task-input");
+let deleteBtns = document.getElementsByClassName('delete-btn')
 
-addButton.addEventListener("click", () => {
-    let taskText = taskField.value;
-    newTaskDiv = document.createElement('div');
-    newTaskDiv.innerHTML = `
-    <div class="task-div">
-            <label for="task-checkbox"><input id="task-checkbox" type="checkbox">${taskText}</label>
-            <button id="button1">x</button>
+
+addButton.addEventListener("click", addTask);
+taskField.addEventListener("keydown", (event) => {
+    if(event.key == 'Enter') {
+        addTask();
+    }
+});
+
+function addTask() {
+    if(taskField.value != '') {
+        let taskText = taskField.value;
+        newTaskDiv = document.createElement('div');
+        newTaskDiv.innerHTML = `
+         <div class="task-div">
+            <label class="text-label" for="checkbox"><input class="checkbox" type="checkbox">${taskText}</label>
+            <button class="delete-btn" onclick="deleteTask(this)">x</button>
+        </div>
     </div>
-        `
-    taskContainer.prepend(newTaskDiv);
-})
+            `
+        taskContainer.prepend(newTaskDiv);
+        taskField.value = ``;
+    } else {
+        alert(`Task field can't be empty!`)
+    }
+};
+
+function deleteTask(element) {
+    let parentElement = element.parentElement;
+    parentElement.remove();
+}
