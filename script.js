7fc8c6757b89dc2dd4d984815af79f437f17d786
Amin Jalal <mo13241234@gmail.com>
2024-10-05 23:19:07 +0300

    Added task add, remove, clear..etc notifications.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const taskField = document.getElementById("new-task-input");
 const clearTaskButton = document.getElementById("clear-tasks-btn");
 let deleteBtns = document.getElementsByClassName('delete-btn');
 let checkboxes = document.getElementsByClassName('checkbox');
+let notificationDiv = document.getElementById('notification-container');
 
 
 addButton.addEventListener("click", addTask);
@@ -28,18 +29,36 @@ function addTask() {
         taskField.value = ``;
         deleteBtns = document.getElementsByClassName('delete-btn');
         checkboxes = document.getElementsByClassName('checkbox');
+        taskAddedNotification();
         addLineThrough();
     } else {
-        alert(`Task field can't be empty!`)
+        // alert(`Task field can't be empty!`)
+        notificationDiv.style.opacity = '100%'
+        notificationDiv.style.fontSize = '1.5rem'
+        notificationDiv.style.backgroundColor = 'red';
+        notificationDiv.style.color = 'darkred';
+        notificationDiv.textContent = 'Task Field cannot be empty!';
+        setTimeout(() => {
+            notificationDiv.style.opacity = '0%';
+        }, 1000);
     }
 };
 
 clearTaskButton.addEventListener("click", (event) => {
     taskContainer.innerHTML = '';
+    notificationDiv.style.opacity = '100%'
+    notificationDiv.style.fontSize = '1.5rem'
+    notificationDiv.style.backgroundColor = '#cb2704';
+    notificationDiv.style.color = 'orange';
+    notificationDiv.textContent = 'All tasks cleared!';
+    setTimeout(() => {
+        notificationDiv.style.opacity = '0%';
+    }, 1000);
 })
 
 function deleteTask(element) {
     let parentElement = element.parentElement;
+    taskDeletedNotification();
     parentElement.remove();
 }
 
@@ -49,9 +68,37 @@ Array.from(checkboxes).forEach((checkbox) => {
     checkbox.addEventListener('change', () => {
         if(checkbox.checked) {
             checkbox.parentElement.classList.add('line-through');
+            let checkboxParent = checkbox.parentElement;
+            checkboxParent.parentElement.style.backgroundColor = 'lightgreen';
+            
         } else {
             checkbox.parentElement.classList.remove('line-through');
+            let checkboxParent = checkbox.parentElement;
+            checkboxParent.parentElement.style.backgroundColor = 'white';
         }
      })
     })
+    
+}
+
+function taskAddedNotification() {
+    notificationDiv.style.opacity = '100%'
+    notificationDiv.style.fontSize = '1.5rem'
+    notificationDiv.style.backgroundColor = 'lightgreen';
+    notificationDiv.style.color = 'darkgreen';
+    notificationDiv.textContent = 'Task Added!';
+    setTimeout(() => {
+        notificationDiv.style.opacity = '0%';
+    }, 1000);
+}
+
+function taskDeletedNotification() {
+    notificationDiv.style.opacity = '100%'
+    notificationDiv.style.fontSize = '1.5rem'
+    notificationDiv.style.backgroundColor = 'red';
+    notificationDiv.style.color = 'darkred';
+    notificationDiv.textContent = 'Task removed!';
+    setTimeout(() => {
+        notificationDiv.style.opacity = '0%';
+    }, 1000);
 }
