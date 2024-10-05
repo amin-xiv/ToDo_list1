dba459de8bc5538f24ca2f38759bec9a3d8ec1c1
Amin Jalal <mo13241234@gmail.com>
2024-10-05 12:40:29 +0300

    Added line through functionality after checking the task.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const addButton = document.getElementById("add-task-btn");
 const taskContainer = document.getElementById("task-container");
 const taskField = document.getElementById("new-task-input");
-let deleteBtns = document.getElementsByClassName('delete-btn')
+let deleteBtns = document.getElementsByClassName('delete-btn');
+let checkboxes = document.getElementsByClassName('checkbox');
 
 
 addButton.addEventListener("click", addTask);
@@ -24,6 +25,9 @@ function addTask() {
             `
         taskContainer.prepend(newTaskDiv);
         taskField.value = ``;
+        deleteBtns = document.getElementsByClassName('delete-btn');
+        checkboxes = document.getElementsByClassName('checkbox');
+        addLineThrough();
     } else {
         alert(`Task field can't be empty!`)
     }
@@ -32,4 +36,17 @@ function addTask() {
 function deleteTask(element) {
     let parentElement = element.parentElement;
     parentElement.remove();
+}
+
+function addLineThrough() {
+    
+Array.from(checkboxes).forEach((checkbox) => {
+    checkbox.addEventListener('change', () => {
+        if(checkbox.checked) {
+            checkbox.parentElement.classList.add('line-through');
+        } else {
+            checkbox.parentElement.classList.remove('line-through');
+        }
+     })
+    })
 }
